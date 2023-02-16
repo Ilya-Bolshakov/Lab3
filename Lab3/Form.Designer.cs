9e059de52fd96c2015cf9931b4226f5bafbabf68
Илья Большаков <gamenest926@gmail.com>
2023-02-16 16:10:13 +0300

    коммит

--- Lab3/Form.Designer.cs
+++ Lab3/Form.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxText = new System.Windows.Forms.ListBox();
+            this.btnRun = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBoxText
+            // 
+            this.listBoxText.FormattingEnabled = true;
+            this.listBoxText.ItemHeight = 16;
+            this.listBoxText.Location = new System.Drawing.Point(12, 12);
+            this.listBoxText.Name = "listBoxText";
+            this.listBoxText.Size = new System.Drawing.Size(776, 244);
+            this.listBoxText.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(308, 319);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(199, 76);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Проверка соединения";
+            this.btnRun.UseVisualStyleBackColor = true;
             // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.listBoxText);
             this.Name = "Form";
             this.Text = "Form";
             this.ResumeLayout(false);
@@ -42,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxText;
+        private System.Windows.Forms.Button btnRun;
     }
 }
 
---
+++ Lab3/Form.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+
+
+namespace Lab3
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        public void ConnectToSql()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=srv\\sql_srv;"
+            + "Initial Catalog=Supply;"
+            + "User ID=sa;"
+            + "Password=sa;";
+            conn.Open();
+            conn.Close();
+        }
+
+    }
+}
